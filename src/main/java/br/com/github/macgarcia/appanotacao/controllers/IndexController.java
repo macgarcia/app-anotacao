8f9d15a9fbe400de979a7fd6c3276b90df643177
Marcos Garcia <marquim.iron@gmail.com>
2022-09-19 23:57:43 -0300

    Update IndexController.java

--- src/main/java/br/com/github/macgarcia/appanotacao/controllers/IndexController.java
+++ src/main/java/br/com/github/macgarcia/appanotacao/controllers/IndexController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.github.macgarcia.appanotacao.entitys.Usuario;
